752fc998e1ba354bbf72595509c220e54311d86d
kretika arora <kretikaarora@gmail.com>
2020-10-24 12:55:08 +0530

    [Kretika Arora] Peforming Enqueue function to create linkedlistqueue

---
+++ DataStructure-LinkedListStackAndQueue/LinkedListQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure_LinkedListStackAndQueue
+{
+   /// <summary>
+   /// creating linkedlist queue
+   /// </summary>   
+    class LinkedListQueue
+    {
+        public Node head;
+
+        /// <summary>
+        /// Adding an element at the top
+        /// UC3
+        /// </summary>
+        /// <param name="newData"></param>
+        public void Enqueue(int newData)
+        {
+            ///queue element can be added from top and removed from bottom (like an actual queue)
+            ///checking if head is null then making it point towards new node
+            ///otherwise creating temp and iterating until it reaches top or null element
+            ///then creating a new link between top element and new element
+            Node newNode = new Node(newData);                    
+            if(this.head==null)
+            {
+                this.head = newNode;             
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("Adding the element " + newNode.data);
+        }    
+
+        /// <summary>
+        /// Displaying values in the LinkedListQueue
+        /// </summary>
+        public void Display()
+        {
+            ///Displaying every element by iterating temp till it is null
+            Console.WriteLine("Display of Values in queue");
+            if (this.head == null)
+            {
+                Console.WriteLine("No element to display , queue is empty");
+                return;
+            }
+            Node temp = this.head;
+            while(temp!=null)
+            {
+                Console.WriteLine("The value in the queue " + temp.data);
+                temp = temp.next;              
+            }
+        }
+    }
+}
--- DataStructure-LinkedListStackAndQueue/Program.cs
+++ DataStructure-LinkedListStackAndQueue/Program.cs
@@ -20,13 +20,12 @@ namespace DataStructure_LinkedListStackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Operations on LinkedList Stack And Queue");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(56);
-            linkedListStack.Push(30);
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.Display();
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+           
         }
     }
 }
